a3b7729f19c7c030b6ba3e33018fc44c34dafa41
JEFERSON DA SILVA SANTOS <jeffersonssantos92@gmail.com>
2026-04-01 09:55:22 -0300

    feat: habilita acesso via rede local (0.0.0.0:8501)

--- iniciar.bat
+++ iniciar.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0"
-venv\Scripts\python.exe venv\Scripts\streamlit.exe  run app.py
+venv\Scripts\python.exe venv\Scripts\streamlit.exe run app.py --server.address=0.0.0.0 --server.port=8501
 pause
--- run_app.py
+++ run_app.py
@@ -1,3 +1,3 @@
 from os import system
 
-system("venv\Scripts\python.exe venv\Scripts\streamlit.exe run app.py")
+system("venv\Scripts\python.exe venv\Scripts\streamlit.exe run app.py --server.address=0.0.0.0 --server.port=8501")
